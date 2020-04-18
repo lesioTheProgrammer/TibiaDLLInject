bd89e6f7e971bae6148d2da8ce4680cd3fc4da9a
Lesio Programista <lama7410@gmail.com>
2020-04-18 15:27:25 +0200

    Add project files.

---
+++ TibiaDLLInject/Tibia_Debug.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TibiaDLLInject
+{
+    public static class Tibia_Debug
+    {
+
+        private static RichTextBox m_logSpace;
+        public static RichTextBox LogSpace
+        {
+            get { return m_logSpace; }
+            private set { m_logSpace = value; }
+        }
+
+        public static void Log(string msg)
+        {
+            m_logSpace.Text += DateTime.Now + "::" + msg + "\n";
+        }
+
+        public static void SetLogSpace(RichTextBox lSpace)
+        {
+            m_logSpace = lSpace;
+        }
+
+
+    }
+
+
+
+}
